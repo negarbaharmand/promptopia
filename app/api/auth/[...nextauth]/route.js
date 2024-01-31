6df75b615b855d4991aad14320c0f733a37e177f
Negar Baharmand <negarbaharmand@outlook.com>
2024-01-31 15:07:46 +0100

    Update route.js to handle the authentication process

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,6 +3,9 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -11,8 +14,35 @@ const handler = NextAuth({
     }),
   ],
 
-  async session({ session }) {},
-  async singIn({ profile }) {},
+  //Every Next.js route is a serverless route. Every time it gets call it needs to make a connection to databse.
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+  },
+  async singIn({ profile }) {
+    try {
+      await connectToDB();
+
+      //check if a user already exists
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+      //if not, create a new user and save it to the databse
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
